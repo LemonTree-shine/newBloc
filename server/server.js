2924cfc6987018f7e1d56c6bf18a2a0e999846af
chenze <chenze@wdai.com>
2018-06-22 14:37:18 +0800

    判断是mac还是linux

--- server/server.js
+++ server/server.js
@@ -178,17 +178,26 @@ process.on('uncaughtException', function (err) {
 //上传文件
 server.post("/upload",function(req,res){
 
- 
-var os = require('os');  
-var IPv4,hostName;  
-hostName=os.hostname();  
-for(var i=0;i<os.networkInterfaces().en0.length;i++){  
-    if(os.networkInterfaces().en0[i].family=='IPv4'){  
-        IPv4=os.networkInterfaces().en0[i].address;  
-    }  
-}  
-console.log('----------local IP: '+IPv4);  
-console.log('----------local host: '+hostName); 
+    console.log(process.platform);
+    var os = require('os');  
+    var IPv4,hostName;  
+    hostName=os.hostname();  
+    if(process.platform == "darwin"){
+        for(var i=0;i<os.networkInterfaces().en0.length;i++){  
+            if(os.networkInterfaces().en0[i].family=='IPv4'){  
+                IPv4=os.networkInterfaces().en0[i].address;  
+            }  
+        }  
+    }else if(process.platform == "linux"){
+        for(var i=0;i<os.networkInterfaces().eth0.length;i++){  
+            if(os.networkInterfaces().eth0[i].family=='IPv4'){  
+                IPv4=os.networkInterfaces().eth0[i].address;  
+            }  
+        } 
+    }
+    
+    console.log('----------local IP: '+IPv4);  
+    console.log('----------local host: '+hostName); 
 
     var pathArr = [];
     const host = app.address().address
